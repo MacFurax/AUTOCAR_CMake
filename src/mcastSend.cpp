496ce0822c13b566312b8a71814a552e65e85064
dupuis <constant.dupuis@gmail.com>
2017-07-21 02:58:56 +0200

    try to compile but fail :-(

--- src/mcastSend.cpp
+++ src/mcastSend.cpp
@@ -1,12 +1,56 @@
 #include <iostream>
 
+#include "mcastSend.hpp"
+
 #include "Poco/Net/MulticastSocket.h"
 #include "Poco/Net/SocketAddress.h"
+#include "Poco/Net/NetException.h"
+#include "Poco/Net/IPAddress.h"
+#include "Poco/Net/IPAddressImpl.h"
+#include "Poco/Timespan.h"
+#include "Poco/Stopwatch.h"
 
-using Poco::Net;
+
+using Poco::Net::Socket;
+using Poco::Net::MulticastSocket;
+using Poco::Net::SocketAddress;
+using Poco::Net::IPAddress;
+using Poco::Timespan;
+using Poco::Stopwatch;
+using Poco::TimeoutException;
+using Poco::InvalidArgumentException;
+using Poco::IOException;
+
+MulticastSocketTest::MulticastSocketTest()
+{
+}
+
+
+MulticastSocketTest::~MulticastSocketTest()
+{
+}
+
+
+void MulticastSocketTest::testMulticast()
+{
+	//MulticastEchoServer echoServer;
+    SocketAddress group("239.255.1.2", 12345);
+	//MulticastSocket ms( SocketAddress::IPv4 );
+    MulticastSocket ms();
+	
+    int n = ms.sendTo("hello", 5, group);
+
+	char buffer[256];
+	n = ms.receiveBytes(buffer, sizeof(buffer));
+
+	ms.close();
+}
+
 
 int main()
 {
-    MulticastSocket ms;
+    MulticastSocketTest mct;
+    mct.testMulticast();
+
     return 0;
 }
---
+++ src/mcastSend.hpp
@@ -0,0 +1,20 @@
+#ifndef MulticastSocketTest_INCLUDED
+#define MulticastSocketTest_INCLUDED
+
+
+#include "Poco/Net/Net.h"
+
+class MulticastSocketTest
+{
+public:
+	MulticastSocketTest();
+	~MulticastSocketTest();
+
+	void testMulticast();
+
+private:
+};
+
+
+
+#endif // MulticastSocketTest_INCLUDED
