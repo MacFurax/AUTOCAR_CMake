a387d36aff28b7ed497314d8116e44d8f84e8f58
dupuis <constant.dupuis@gmail.com>
2017-07-23 22:28:17 +0200

    add OKAR::node header

---
+++ src/OKAR/node.hpp
@@ -0,0 +1,39 @@
+#ifndef OKAR_NODE
+#define OKAR_NODE
+
+#include <string>
+
+using namespace std;
+
+namespace OKAR
+{
+
+class Node 
+{
+public:
+
+    Node() {}
+    Node( string name, string ip) {}    
+    ~Node() {}
+
+    run() {
+        
+        // create sockets
+
+        // loop until stoped
+        //  every 1 sec send registring/HeartBeat packet
+        //  check for incomming packet
+        //  treat incoming packet
+
+        // close socket
+
+    }
+
+
+private:
+
+};
+
+}// namespace OKAR
+
+#endif // OKAR_NODE
